de55554b34904a4b41bdab4261d88b43412af65b
Gent B <gentblaku@gmail.com>
2021-02-04 16:54:16 -0500

    Date Class implentation + Test

--- Project 1/src/Date.java
+++ Project 1/src/Date.java
@@ -1,10 +1,50 @@
+import java.util.StringTokenizer;
+import java.util.Calendar;
+
 
 public class Date {
 	private int year;
 	private int month;
 	private int day;
-	public Date(String date) { } //taking mm/dd/yyyy and create a Date object
-	public Date() { } //create an object with today’s date (see Calendar class)
+	public Date(String date) {	//taking mm/dd/yyyy and create a Date object
+		StringTokenizer tokenizer = new StringTokenizer(date, "/");
+		this.month = Integer.parseInt(tokenizer.nextToken());
+		this.day   = Integer.parseInt(tokenizer.nextToken());
+		this.year  = Integer.parseInt(tokenizer.nextToken());	
+		
+	} 
+	/*
+	public void main(String[] args) {
+		Date date = new Date();
+		System.out.println(this.month);
+		System.out.println(this.day);
+		System.out.println(this.year);
+	}
+	*/
+	
+	public Date() { 
+		Calendar today = Calendar.getInstance();
+		this.month = today.get(Calendar.MONTH);
+		this.day   = today.get(Calendar.DATE);
+		this.year  = today.get(Calendar.YEAR);
+		
+	} //create an object with today’s date (see Calendar class)
+	
 	
 	//public boolean isValid() { }
+	
+	/*
+	@Override
+	public boolean equals(Object obj){ }
+	*/
+	@Override
+	
+	public String toString() { 
+		//make this prettier
+		return String.valueOf(this.month) + String.valueOf(this.day) + String.valueOf(this.year);
+	}
+	
+	
+	
+	
 }
--- Project 1/src/RunProject1.java
+++ Project 1/src/RunProject1.java
@@ -1,6 +1,15 @@
 
+
 public class RunProject1 {
-	public static void main(String[] args) {
-		new Kiosk().run();
+	//public static void main(String[] args) {
+		//new Kiosk().run();
+//	}
+	
+	public void main(String[] args) {
+		Date date = new Date();
+		System.out.println(date.toString());
 	}
+	
+	
+	
 }
