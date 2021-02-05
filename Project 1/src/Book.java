a2a1479de5b31abe7a349ffd274df9b19bd574ea
Gent B <gentblaku@gmail.com>
2021-02-05 13:15:37 -0500

    Should be up to date now

--- Project 1/src/Book.java
+++ Project 1/src/Book.java
@@ -12,6 +12,7 @@ public class Book {
 		this.name   = bookName;
 		Date date = new Date(publishDate);
 		
+		// This should be called in Library.java
 		//if (!date.isValid()) {
 			//THIS HAS TO BE A PRINT "Invalid Date!"
 		//	return;
